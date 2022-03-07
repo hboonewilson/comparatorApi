1f691b45b7b10b4d5098fc3659dd9104b15f467f
hboonewilson <hbwilson@uiowa.edu>
2022-03-07 11:54:29 -0600

    Revert "WIP, constructing and finding paralells is harder with 3 nested lists. //todo create comparator class and possibly newtork class to hide complexity"
    This reverts commit 7d8833ff8aba4551353987765470cb2192af4b10.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { response } = require('express')
 const express = require('express')
 const { json } = require('express/lib/response')
 const comparatorNetworks = require('./comparatorNetworks')
-const paralell = require('./comparatorNetworks/parallel')
 const app = express();
 const PORT = process.env.PORT || 3000
 
@@ -24,14 +23,9 @@ app.get('/sort', (req, res) => {
     res.send("send with valid argument: arr=[a,b,c]")
   }
 })
-app.get('/parallel', (req, res) => {
-  let arr = comparatorNetworks.tupleToArray(req.query.arr);
-
-})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
-paralell.insertArrIntoParallelLis([1,2], [[[1,2],[2,3]],[3,4]])
 
 
-//https://comparator-api.herokuapp.com/
+//https://infinite-cove-72975.herokuapp.com/
